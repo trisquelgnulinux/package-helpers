57470b09a4e1c6970914fa3c7be59f396435f708
Rubén Rodríguez <ruben@trisquel.info>
2012-09-09 01:02:29 +0200

    Initial release for Dagda

---
+++ helpers/DATA/firefox/branding/pref/abrowser-branding.js
@@ -0,0 +1,15 @@
+pref("startup.homepage_override_url","http://trisquel.info");
+pref("startup.homepage_welcome_url","http://trisquel.info/welcome");
+// The time interval between checks for a new version (in seconds)
+// nightly=8 hours, official=24 hours
+pref("app.update.interval", 86400);
+// The time interval between the downloading of mar file chunks in the
+// background (in seconds)
+pref("app.update.download.backgroundInterval", 60);
+// URL user can browse to manually if for some reason all update installation
+// attempts fail.
+pref("app.update.url.manual", "http://trisquel.info/browser");
+// A default value for the "More information about this update" link
+// supplied in the "An update is available" page of the update wizard. 
+pref("app.update.url.details", "http://trisquel.info/browser");
+
