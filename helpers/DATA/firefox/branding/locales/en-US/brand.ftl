2c3bea184a276e25a7695674479ec39a245e7157
Ruben Rodriguez <ruben@trisquel.info>
2018-05-13 17:43:53 -0400

    Abrowser: branding and other changes for v60

--- helpers/DATA/firefox/branding/locales/en-US/brand.ftl
+++ helpers/DATA/firefox/branding/locales/en-US/brand.ftl
@@ -2,9 +2,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-brandShorterName=Abrowser
-brandShortName=Abrowser
-brandFullName=Abrowser
-vendorShortName=Trisquel
-
-syncBrandShortName=Sync
+-brand-short-name = Abrowser
--- helpers/DATA/firefox/branding/pref/abrowser-branding.js
+++ helpers/DATA/firefox/branding/pref/abrowser-branding.js
@@ -2,27 +2,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("startup.homepage_override_url","http://trisquel.info");
-pref("startup.homepage_welcome_url","http://trisquel.info/welcome");
-// The time interval between checks for a new version (in seconds)
-// nightly=8 hours, official=24 hours
-pref("app.update.interval", 86400);
+pref("startup.homepage_override_url","https://trisquel.info");
+pref("startup.homepage_welcome_url","https://trisquel.info/welcome");
+pref("startup.homepage_welcome_url.additional", "");
+// Interval: Time between checks for a new version (in seconds)
+pref("app.update.interval", 43200); // 12 hours
 // The time interval between the downloading of mar file chunks in the
 // background (in seconds)
-pref("app.update.download.backgroundInterval", 60);
+// 0 means "download everything at once"
+pref("app.update.download.backgroundInterval", 0);
+// Give the user x seconds to react before showing the big UI. default=192 hours
+pref("app.update.promptWaitTime", 691200);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "http://trisquel.info/browser");
+pref("app.update.url.manual", "https://trisquel.info/browser");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard.
-pref("app.update.url.details", "http://trisquel.info/browser");
+pref("app.update.url.details", "https://trisquel.info/browser");
 
-// Release notes and vendor URLs
-pref("app.releaseNotesURL", "http://trisquel.info/browser");
 pref("app.vendorURL", "http://trisquel.info/browser/");
+pref("app.releaseNotesURL", "https://trisquel.info/browser");
 
-// Search codes belong only in builds with official branding
-pref("browser.search.param.yahoo-fr", "");
-pref("browser.search.param.yahoo-fr-cjkt", ""); // now unused
-pref("browser.search.param.yahoo-fr-ja", "");
-pref("browser.search.param.yahoo-f-CN", "");
+// The number of days a binary is permitted to be old
+// without checking for an update.  This assumes that
+// app.update.checkInstallTime is true.
+pref("app.update.checkInstallTime.days", 63);
+
+// Give the user x seconds to reboot before showing a badge on the hamburger
+// button. default=4 days
+pref("app.update.badgeWaitTime", 345600);
+
+// Number of usages of the web console or scratchpad.
+// If this is less than 5, then pasting code into the web console or scratchpad is disabled
+pref("devtools.selfxss.count", 0);
