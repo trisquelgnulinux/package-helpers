405fb1b8bfa290c9fd0cdf40cd6fa27502110f8b
Luis Guzmán <ark@switnet.org>
2021-07-12 23:15:05 +0000

    Base installer

--- helpers/DATA/base-installer/amd64.sh
+++ helpers/DATA/base-installer/amd64.sh
@@ -14,18 +14,24 @@ arch_get_kernel () {
 	echo "linux-generic"
 	echo "linux-image-generic"
 
-	echo "linux-server"
-	echo "linux-image-server"
+	echo "linux-generic-hwe-20.04"
+	echo "linux-image-generic-hwe-20.04"
 
-	echo "linux-virtual"
-	echo "linux-image-virtual"
+        echo "linux-lowlatency"
+        echo "linux-image-lowlatency"
 
-	echo "linux-xen"
-	echo "linux-image-xen"
+        echo "linux-lowlatency-hwe-20.04"
+        echo "linux-image-lowlatency-hwe-20.04"
 
-	echo "linux-preempt"
-	echo "linux-image-preempt"
+        echo "linux-oem-20.04"
+        echo "linux-image-oem-20.04"
 
-	echo "linux-rt"
-	echo "linux-image-rt"
+        echo "linux-virtual"
+        echo "linux-image-virtual"
+
+        echo "linux-image-extra-virtual"
+	echo "linux-virtual-hwe-20.04"
+
+	echo "linux-image-virtual-hwe-20.04"
+	echo "linux-image-extra-virtual-hwe-20.04"
 }
