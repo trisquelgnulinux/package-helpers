ac94a07ee28736954d4403d0ff9ce62be29d6f0a
Luis Guzmán <ark@switnet.org>
2025-04-19 02:00:31 +0000

    base-installer: TESTING release

--- helpers/DATA/base-installer/archs/amd64.sh
+++ helpers/DATA/base-installer/archs/amd64.sh
@@ -3,7 +3,7 @@ arch_get_kernel_flavour () {
 }
 
 arch_check_usable_kernel () {
-	if echo "$1" | grep -q -e "signed" -e "edge" -e "hwe-16.04"; then return 1; fi
+	if echo "$1" | grep -q -e "signed" -e "edge" -e "hwe-@REVISION@"; then return 1; fi
 	if echo "$1" | grep -Eq -- "-(server|generic|virtual|xen|preempt|rt)(-.*)?$"; then return 0; fi
 
 	return 1
@@ -14,24 +14,24 @@ arch_get_kernel () {
 	echo "linux-generic"
 	echo "linux-image-generic"
 
-	echo "linux-generic-hwe-20.04"
-	echo "linux-image-generic-hwe-20.04"
+	echo "linux-generic-hwe-@REVISION@"
+	echo "linux-image-generic-hwe-@REVISION@"
 
         echo "linux-lowlatency"
         echo "linux-image-lowlatency"
 
-        echo "linux-lowlatency-hwe-20.04"
-        echo "linux-image-lowlatency-hwe-20.04"
+        echo "linux-lowlatency-hwe-@REVISION@"
+        echo "linux-image-lowlatency-hwe-@REVISION@"
 
-        echo "linux-oem-20.04"
-        echo "linux-image-oem-20.04"
+        echo "linux-oem-@REVISION@"
+        echo "linux-image-oem-@REVISION@"
 
         echo "linux-virtual"
         echo "linux-image-virtual"
 
         echo "linux-image-extra-virtual"
-	echo "linux-virtual-hwe-20.04"
+	echo "linux-virtual-hwe-@REVISION@"
 
-	echo "linux-image-virtual-hwe-20.04"
-	echo "linux-image-extra-virtual-hwe-20.04"
+	echo "linux-image-virtual-hwe-@REVISION@"
+	echo "linux-image-extra-virtual-hwe-@REVISION@"
 }
